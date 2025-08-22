1c83e68bf4de82f80782beb3078c3fc2fc89a1ee
M Gulab Yar <mgulabyaarmgulabyaar@gmail.com>
2025-08-22 07:57:55 +0500

    URL pushed

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
 
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; 
+const urlProd = "https://outlook-add-in-xi.vercel.app/"; 
 
 async function getHttpsOptions() {
   const httpsOptions = await devCerts.getHttpsServerOptions();
